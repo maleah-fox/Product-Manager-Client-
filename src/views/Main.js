3fc4cb538160027682825de2632371a2dfc1111a
maleah-fox <tarhataobrien@gmail.com>
2021-05-20 19:46:57 -0700

    Updated to display from entries

---
+++ src/views/Main.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import ProductForm from '../components/ProductForm';
+import ProductList from '../components/ProductList';
+const Main = () => {
+    const [products, setProducts] = useState([]);
+    const [loaded, setLoaded] = useState(false);
+    useEffect(()=>{
+        axios.get('http://localhost:8000/api/product')
+            .then(res=>{
+                setProducts(res.data);
+                setLoaded(true);
+            });
+    },[])
+    return (
+        <div>
+           <ProductForm/>
+           <hr/>
+           {loaded && <ProductList products={products}/>}
+        </div>
+    )
+}
+
+export default Main;
